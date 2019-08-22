214b7c833032d6d6e38e67cba41163e6a1be7ed7
neko3cs <minaton.0722.net@gmail.com>
2019-08-22 22:14:49 +0900

    update: 不要な処理の削除

--- brew-install.sh
+++ brew-install.sh
@@ -2,25 +2,6 @@
 
 brew update
 
-outdated=$(brew outdated)
-
-if [ -n "$outdated" ]; then
-    cat << EOF
-
-The following package(s) will upgrade.
-$outdated
-
-Are you sure?
-If you don't want to upgrade, please type Ctrl-c now.
-EOF
-
-  read dummy
-
-  brew cleanup
-  brew upgrade
-
-fi
-
 brew install automake
 brew install azure-cli
 brew tap azure/functions; brew install azure-functions-core-tools
