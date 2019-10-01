e6f23f39ce4ab387fad57c90bdae32af5f85faeb
neko3cs <minaton.0722.net@gmail.com>
2019-10-01 15:43:27 +0900

    add: vscode-install for windows

---
+++ vscode-install.ps1
@@ -0,0 +1,10 @@
+# Windows PowerShell
+
+code --install-extension ms-ceintl.vscode-language-pack-ja
+code --install-extension ms-vscode.csharp
+code --install-extension visualstudioexptteam.vscodeintellicode
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension ms-vscode.powershell
+code --install-extension ms-vscode.azure-account
+code --install-extension ms-mssql.mssql
+code --install-extension gruntfuggly.todo-tree
