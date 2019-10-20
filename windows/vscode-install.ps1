8d61779736fc7829b77bf1cf7689c898ff292ae8
neko3cs <minaton.0722.net@gmail.com>
2019-10-20 19:30:24 +0900

    インストール対象を配列化

--- windows/vscode-install.ps1
+++ windows/vscode-install.ps1
@@ -1,10 +1,16 @@
 # Windows PowerShell
 
-code --install-extension ms-ceintl.vscode-language-pack-ja
-code --install-extension ms-vscode.csharp
-code --install-extension visualstudioexptteam.vscodeintellicode
-code --install-extension ms-vsliveshare.vsliveshare
-code --install-extension ms-vscode.powershell
-code --install-extension ms-vscode.azure-account
-code --install-extension ms-mssql.mssql
-code --install-extension gruntfuggly.todo-tree
+$packages = @(
+    "ms-ceintl.vscode-language-pack-ja",
+    "ms-vscode.csharp",
+    "visualstudioexptteam.vscodeintellicode",
+    "ms-vsliveshare.vsliveshare",
+    "ms-vscode.powershell",
+    "ms-vscode.azure-account",
+    "ms-mssql.mssql",
+    "gruntfuggly.todo-tree"
+)
+
+foreach ($package in $packages) {
+    code --install-extension $package
+}
