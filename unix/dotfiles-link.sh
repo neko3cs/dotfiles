601114f1612b4ba50be00fdf1c444b2552892eca
neko3cs <minaton.0722.net@gmail.com>
2019-10-20 22:13:04 +0900

    パスが間違ってるので修正

--- unix/dotfiles-link.sh
+++ unix/dotfiles-link.sh
@@ -10,5 +10,5 @@ for dotfile in "${dotfiles[@]}"; do
   ln -sf ~/dotfiles/unix/$dotfile ~/$dotfile
 done
 
-source .zprofile
-source .zshrc
+source ~/.zprofile
+source ~/.zshrc # TODO: maybe, is it unnecessary?
