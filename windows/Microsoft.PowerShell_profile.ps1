cf8cf90590595a9d59c5728e90c9e4ad67c39ac7
neko3cs <minaton.0722.net@gmail.com>
2019-10-22 19:01:17 +0900

    pwsh_profileもスクリプトで配置できるようにした

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -2,5 +2,6 @@
 
 Set-Alias -Name acrobat -Value 'C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe'
 Set-Alias -Name open -Value 'C:\Windows\explorer.exe'
+# Set-Alias -Name visualstudio -Value "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
 Set-Alias -Name visualstudio -Value "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
 Set-Alias -Name ll -Value Get-ChildItem
--- windows/dotfiles-link.ps1
+++
@@ -1,5 +0,0 @@
-# Windows Powershell
-
-# TODO: 連想配列で管理
-# TODO: 実態があるとResourceExistsエラーするので事前に削除する　->　本当に消してよいか確認
-New-Item -Value .\Microsoft.PowerShell_profile.ps1 -Path $env:USERPROFILE\Documents\PowerShell\ -Name 'Microsoft.PowerShell_profile.ps1' -ItemType SymbolicLink
