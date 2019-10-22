0170ad8ee373aac47c624290187d7b0481b509c0
neko3cs <minaton.0722.net@gmail.com>
2019-10-22 17:32:44 +0900

    引数でプライベートと仕事用のパッケージコンフィグを指定できるように修正

--- windows/chocolatey-install.ps1
+++ windows/chocolatey-install.ps1
@@ -1,11 +1,35 @@
 # Windows PowerShell
 
+Param (
+    [ValidateScript( { [Enum]::GetValues([UseFor]) -contains $_ })]
+    [string]$UseFor
+)
+
+enum UseFor {
+    Private
+    Work
+}
+
 $ExecutionPolicy = Get-ExecutionPolicy
 if ($ExecutionPolicy -eq 'RemoteSigned') {
     Set-ExecutionPolicy RemoteSigned
 }
 
-Set-ExecutionPolicy Bypass -Scope Process -Force
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+if (!(Get-Command choco -ea SilentlyContinue)) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
 
 choco install chocolatey-package.config -y
+
+switch ($UseFor) {
+    UseFor.Private { 
+        choco install chocolatey-package-private.config -y
+    }
+    UseFor.Work {
+        choco install chocolatey-package-work.config -y
+    }
+    Default {
+        choco install chocolatey-package-private.config -y
+    }
+}
