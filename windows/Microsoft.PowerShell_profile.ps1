7744a116d88e26dbd3f9e693b6f77c67dc9945bf
neko3cs <minaton.0722.net@gmail.com>
2020-01-02 21:53:10 +0900

    pwsh-profileの複数端末対応

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -2,10 +2,15 @@
 
 Set-Alias -Name acrobat -Value 'C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe'
 Set-Alias -Name open -Value 'C:\Windows\explorer.exe'
-# Set-Alias -Name visualstudio -Value "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
-Set-Alias -Name visualstudio -Value "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
 Set-Alias -Name ll -Value Get-ChildItem
 function Get-ChildItem-All {
     Get-ChildItem -Force
 }
 Set-Alias -Name la -Value Get-ChildItem-All
+
+if ($env:COMPUTERNAME -eq "NEKO3CS-PARALLE") {
+    Set-Alias -Name visualstudio -Value "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
+}
+else {
+    Set-Alias -Name visualstudio -Value "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
+}
