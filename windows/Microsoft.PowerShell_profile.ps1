03ccb6a0b5171a15691bb98e341497f3866dab58
neko3cs <minaton.0722.net@gmail.com>
2020-01-16 13:58:38 +0900

    add winmerge command

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -8,9 +8,12 @@ function Get-ChildItem-All {
 }
 Set-Alias -Name la -Value Get-ChildItem-All
 
-if ($env:COMPUTERNAME -eq "NEKO3CS-PARALLE") {
-    Set-Alias -Name visualstudio -Value "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
-}
-else {
-    Set-Alias -Name visualstudio -Value "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
+switch ($env:COMPUTERNAME) {
+    "NEKO3CS-PARALLE" { 
+        Set-Alias -Name visualstudio -Value "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
+    }
+    Default { 
+        Set-Alias -Name visualstudio -Value "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
+        Set-Alias -Name winmerge -Value "C:\Program Files\WinMerge\WinMergeU.exe"
+    }
 }
