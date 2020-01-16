31d7068cd2d026fccee68828b422b3d37c1defae
neko3cs <minaton.0722.net@gmail.com>
2020-01-16 13:58:56 +0900

    remove param

--- windows/pwsh-profile-link.ps1
+++ windows/pwsh-profile-link.ps1
@@ -1,13 +1,8 @@
 # Windows Powershell
 
-Param (
-    [Parameter(Mandatory)]
-    [string]$ProfilePath
-)
-
-if (Test-Path $ProfilePath) {
-    Remove-Item -Force $ProfilePath
+if (Test-Path $PROFILE) {
+    Remove-Item -Force $PROFILE
 }
 
 # Can not 'New-Item -ItemType SymbolicLink'; it's specified file, so copy this.
-Copy-Item .\Microsoft.PowerShell_profile.ps1 $ProfilePath
+Copy-Item .\Microsoft.PowerShell_profile.ps1 $PROFILE
