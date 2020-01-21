8feceac7d468ffa75609604f1355246fc7ab597f
neko3cs <minaton.0722.net@gmail.com>
2020-01-22 01:52:35 +0900

    homebrew caskのインストールを別スクリプト化

---
+++ brew-cask-install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+type brew >/dev/null 2>&1 && {
+  echo "  command 'brew' not found!"
+  echo "  please run 'brew-install.sh' before this."
+  exit;
+}
+
+brew doctor
+brew update
+brew upgrade
+
+taps=(
+)
+
+casks=(
+  amethyst
+  appcleaner
+  docker
+  google-chrome
+  github
+  hub
+  java
+  kindle
+  powershell
+  springboot
+  visual-studio-code
+  visual-studio
+)
+
+for tap in "${taps[@]}"; do
+  brew tap $tap
+done
+
+for cask in "${casks[@]}"; do
+  brew cask install $cask || brew cask upgrade $cask
+done
+
+brew cleanup
