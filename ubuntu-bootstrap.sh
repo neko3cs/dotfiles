419cc61f5ae895eb2ab74263cdc3a9740ef8cbd1
neko3cs <minaton.0722.net@gmail.com>
2020-01-24 00:24:54 +0900

    functionize 'ubuntu-bootstrap.sh'

--- ubuntu-bootstrap.sh
+++ ubuntu-bootstrap.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
 
+activate_ubuntu() {
+  # change dash->bash by selecting <No>
+  sudo dpkg-reconfigure dash
+  # japanese lang pack
+  sudo apt install -y language-pack-ja
+  # locale japanese
+  sudo update-locale LANG=ja_JP.UTF-8
+  # timezone azia/tokyo
+  sudo dpkg-reconfigure tzdata
+  # japanese man
+  sudo apt install -y manpages-ja manpages-ja-dev
+}
+
+install_linuxbrew() {
+  sudo apt install build-essential curl file git
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+  test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+  test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+  echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+}
+
 sudo apt update
 sudo apt upgrade
 
-# change dash->bash by selecting <No>
-sudo dpkg-reconfigure dash
-# japanese lang pack
-sudo apt install -y language-pack-ja
-# locale japanese
-sudo update-locale LANG=ja_JP.UTF-8
-# timezone azia/tokyo
-sudo dpkg-reconfigure tzdata
-# japanese man
-sudo apt install -y manpages-ja manpages-ja-dev
-
-# install homebrew
-sudo apt install build-essential curl file git
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-
+activate_ubuntu
+install_linuxbrew
--- xcode-cli-tools-install.sh
+++ xcode-cli-tools-install.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 xcode-select --install
-
