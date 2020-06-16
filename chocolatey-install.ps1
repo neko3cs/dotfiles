d2b9e77a8d4daaa3cf6421071a69055950f581f5
neko3cs <minaton.0722.net@gmail.com>
2020-06-16 15:24:59 +0900

    abolish chocolately-package.private.config and chocolately-package.work.config: merge to chocolately-package.config

--- chocolatey-install.ps1
+++ chocolatey-install.ps1
@@ -1,14 +1,4 @@
-# Windows PowerShell
-
-Param (
-    [ValidateScript( { [Enum]::GetValues([UseFor]) -contains $_ })]
-    [string]$UseFor = "Private"
-)
-
-enum UseFor {
-    Private
-    Work
-}
+#!pwsh
 
 $ExecutionPolicy = Get-ExecutionPolicy
 if ($ExecutionPolicy -eq 'RemoteSigned') {
@@ -20,10 +10,4 @@ if (!(Get-Command choco -ea SilentlyContinue)) {
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
-choco install chocolatey-package.config -y
-
-switch ($UseFor) {
-    UseFor.Private { choco install chocolatey-package.private.config -y }
-    UseFor.Work { choco install chocolatey-package.work.config -y }
-    Default { choco install chocolatey-package.private.config -y }
-}
+choco install chocolatey-package.config --yes
