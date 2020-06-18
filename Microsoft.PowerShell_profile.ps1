45618b28523e25e19092b9e2f4eca1361ffc51bc
neko3cs <minaton.0722.net@gmail.com>
2020-06-19 01:12:34 +0900

    crossplatforming Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,23 @@
 #!pwsh
 
-Set-Alias -Name acrobat -Value 'C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe'
-Set-Alias -Name open -Value 'C:\Windows\explorer.exe'
+if ($IsWindows) {
+    Set-Alias -Name acrobat -Value 'C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe'
+    Set-Alias -Name open -Value 'C:\Windows\explorer.exe'
+    
+    switch ($env:COMPUTERNAME) {
+        "NEKO3CS-PARALLE" { 
+            Set-Alias -Name visualstudio -Value "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
+        }
+        Default { 
+            Set-Alias -Name visualstudio -Value "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
+            Set-Alias -Name winmerge -Value "C:\Program Files\WinMerge\WinMergeU.exe"
+        }
+    }
+}
+
 Set-Alias -Name ll -Value Get-ChildItem
-function Get-ChildItem-All {
+function Get-ChildItemAll {
     Get-ChildItem -Force
 }
-Set-Alias -Name la -Value Get-ChildItem-All
+Set-Alias -Name la -Value Get-ChildItemAll
 Set-Alias -Name touch -Value New-Item
-
-switch ($env:COMPUTERNAME) {
-    "NEKO3CS-PARALLE" { 
-        Set-Alias -Name visualstudio -Value "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
-    }
-    Default { 
-        Set-Alias -Name visualstudio -Value "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
-        Set-Alias -Name winmerge -Value "C:\Program Files\WinMerge\WinMergeU.exe"
-    }
-}
