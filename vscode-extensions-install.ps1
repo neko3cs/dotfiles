9985fe0631347ae8bad486a277d7bd9dbe1120b4
neko3cs <minaton.0722.net@gmail.com>
2020-06-18 18:49:58 +0900

    add vscode extensions installed macOS

--- vscode-extensions-install.ps1
+++ vscode-extensions-install.ps1
@@ -1,13 +1,20 @@
 #!pwsh
 
 $extensions = @(
+    "angular.ng-template"
     "ms-vscode.azure-account"
     "ms-vscode.azurecli"
+    "ms-vscode.cpptools"
     "ms-dotnettools.csharp"
     "ms-vscode.cmake-tools"
+    "ms-azuretools.vscode-docker"
+    "golang.go"
     "ms-ceintl.vscode-language-pack-ja"
     "ms-vsliveshare.vsliveshare"
+    "yzane.markdown-pdf"
+    "jebbs.plantuml"
     "ms-vscode.powershell"
+    "humao.rest-client"
     "ms-mssql.mssql"
     "jtlowe.vscode-icon-theme"
     "gruntfuggly.todo-tree"
