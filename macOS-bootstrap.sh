538fdd6a7d37cdc4f79674759db1a774fac93cc4
neko3cs <minaton.0722.net@gmail.com>
2020-06-20 17:00:52 +0900

    change run order.

--- macOS-bootstrap.sh
+++ macOS-bootstrap.sh
@@ -9,7 +9,7 @@ cd .dotfiles
 xcode-select --install
 
 sh ./dotfiles-link.sh
-sh ./vim-install.sh
 sh ./brew-install.sh
 sh ./brew-cask-install.sh
+sh ./vim-install.sh
 pwsh ./vscode-extensions-install.ps1
