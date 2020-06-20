50d590672f2aa0eb929805add6416f5bb688e1f7
neko3cs <minaton.0722.net@gmail.com>
2020-06-20 16:43:00 +0900

    remove wasteful process

--- chocolatey-install.ps1
+++ chocolatey-install.ps1
@@ -4,11 +4,6 @@ param(
     [switch]$Force
 )
 
-$ExecutionPolicy = Get-ExecutionPolicy
-if ($ExecutionPolicy -eq 'RemoteSigned') {
-    Set-ExecutionPolicy RemoteSigned
-}
-
 if (!(Get-Command choco -ea SilentlyContinue)) {
     Set-ExecutionPolicy Bypass -Scope Process -Force
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
