813abf1356391ff9a35c91950c3cb592f33b122c
neko3cs <minaton.0722.net@gmail.com>
2020-07-07 14:55:01 +0900

    change path name: /dotfiles -> $PWD

--- vim-install.ps1
+++ vim-install.ps1
@@ -7,5 +7,4 @@ if (-not (Test-Path $HOME\.vim\bundle\neobundle.vim\)) {
 if (Test-Path $HOME\.vimrc) {
   Remove-Item $HOME\.vimrc
 }
-Copy-Item $HOME\dotfiles\.vimrc $HOME\.vimrc
-
+Copy-Item $PWD\.vimrc $HOME\.vimrc
