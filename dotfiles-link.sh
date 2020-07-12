3396977f64df633069ec31af53dcc449a6f08a5d
neko3cs <minaton.0722.net@gmail.com>
2020-07-12 18:07:17 +0900

    source ~/.bash_login

--- dotfiles-link.sh
+++ dotfiles-link.sh
@@ -8,12 +8,6 @@ dotfiles=(
   .zlogin
 )
 
-# wsl2 ubuntu settings
-os_name=`awk -F= '$1=="NAME" { print $2 ;}' /etc/os-release | sed 's/"//g'`
-if [ $os_name = "Ubuntu" ]; then
-  dotfiles+=".bash_login"
-fi
-
 for dotfile in "${dotfiles[@]}"; do
   ln -sf $(pwd)/$dotfile ~/$dotfile
 done
@@ -21,3 +15,10 @@ done
 source ~/.zprofile
 source ~/.zshrc
 source ~/.zlogin
+
+# wsl2 ubuntu settings
+os_name=`awk -F= '$1=="NAME" { print $2 ;}' /etc/os-release | sed 's/"//g'`
+if [ $os_name = "Ubuntu" ]; then
+  ln -sf $(pwd)/.bash_login ~/.bash_login
+  source ~/.bash_login
+fi
