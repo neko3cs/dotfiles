215a216119ba3aeedb55d4dddfcc9685eda551c6
neko3cs <minaton.0722.net@gmail.com>
2020-07-12 18:22:51 +0900

    delete run zsh process: use 'wsl zsh'

--- dotfiles-link.sh
+++ dotfiles-link.sh
@@ -15,10 +15,3 @@ done
 source ~/.zprofile
 source ~/.zshrc
 source ~/.zlogin
-
-# wsl2 ubuntu settings
-os_name=`awk -F= '$1=="NAME" { print $2 ;}' /etc/os-release | sed 's/"//g'`
-if [ $os_name = "Ubuntu" ]; then
-  ln -sf $(pwd)/.bash_login ~/.bash_login
-  source ~/.bash_login
-fi
