36cc7ff3f94d7562769b7eb29f2cb1fde9be6903
neko3cs <minaton.0722.net@gmail.com>
2020-07-13 11:48:24 +0900

    add manual install zsh-completion > apt-install.sh

--- apt-install.sh
+++ apt-install.sh
@@ -15,5 +15,9 @@ for pkg in "${pkgs[@]}"; do
   sudo apt install $pkg || sudo apt upgrade $pkgs
 done
 
+git clone git://github.com/zsh-users/zsh-completions.git ~/zsh-completions
+fpath=(~/zsh-completions/src $fpath)
+rm -f ~/.zcompdump; compinit
+
 sudo apt autoremove
 sudo apt autoclean
