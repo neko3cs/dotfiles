994e3cfb22eb4522dfa100ba97d619ac4e42cf76
neko3cs <minaton.0722.net@gmail.com>
2021-02-09 13:09:20 +0900

    merge brew-cask-install.sh into brew-install.sh

--- brew-install.sh
+++ brew-install.sh
@@ -39,12 +39,33 @@ formulas=(
   zsh-completions
 )
 
+casks=(
+  appcleaner
+  discord
+  docker
+  dropbox
+  google-chrome
+  google-japanese-ime
+  github
+  java
+  mysqlworkbench
+  notion
+  powershell
+  unity-hub
+  visual-studio-code
+  visual-studio
+)
+
 for tap in "${taps[@]}"; do
   brew tap $tap
 done
 
 for formula in "${formulas[@]}"; do
   brew install $formula || brew upgrade $formula
+done
+
+for cask in "${casks[@]}"; do
+  brew install --cask $cask || brew upgrade --cask $cask
 done
 
 brew cleanup
