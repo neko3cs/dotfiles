564cb2a0396eaa1283947f3dafd8b2bb5fca1903
neko3cs <minaton.0722.net@gmail.com>
2021-05-04 15:37:00 +0900

    rewrite vim setting script

--- Set-VimConfigs.ps1
+++ Set-VimConfigs.ps1
@@ -1,12 +1,35 @@
 #!pwsh
-#
-# NeoBundle Vim Install Script
-# Source: https://github.com/Shougo/neobundle.vim
-#
 
-if (Test-Path -Path "$HOME/.vim/bundle/") {
-  Remove-Item -Recurse -Path "$HOME/.vim/bundle/"
+[CmdletBinding()]
+param (
+  [Parameter()]
+  [switch]
+  $ReplaceVimRc,
+  [Parameter()]
+  [switch]
+  $Force
+)
+
+if ($Force) {
+  if (Test-Path -Path $HOME/.vim/bundle/) {
+    Remove-Item `
+      -Path $HOME/.vim/bundle/ `
+      -Recurse `
+      -Force
+  }
+  if (Test-Path -Path $HOME/.vimrc) {
+    Remove-Item `
+      -Path $HOME/.vimrc `
+      -Force
+  }
 }
 
-mkdir $HOME/.vim/bundle/
-git clone https://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
+if (-not (Test-Path -Path $HOME/.vim/bundle/)) {
+  mkdir $HOME/.vim/bundle/
+  git clone https://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
+}
+if ((-not (Test-Path -Path $HOME/.vimrc)) -or $ReplaceVimRc) {
+  Copy-Item `
+    -Path $HOME\.dotfiles\.vimrc `
+    -Destination $HOME\.vimrc
+}
