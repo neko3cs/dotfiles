5b73f42cba4273588a86b91dd4b15ee85d177214
neko3cs <minaton.0722.net@gmail.com>
2021-06-02 04:12:09 +0900

    check for run as administraor

--- windows-bootstrap.ps1
+++ windows-bootstrap.ps1
@@ -1,18 +1,13 @@
 # Windows PowerShell
 
-# Confirmation to run
-$title = "clone .dotfiles repo and run bootstrap scripts."
-$message = "ok?: "
-$options = [System.Management.Automation.Host.ChoiceDescription[]](
-    (New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "実行する"),
-    (New-Object System.Management.Automation.Host.ChoiceDescription "&No", "実行しない")
-)
-$result = $Host.UI.PromptForChoice($title, $message, $options, 1)
-if ($result -ne 0) { exit } 
-
+# Check for run as administrator
+[Security.Principal.WindowsPrincipal]$CurrentPrincipal = [Security.Principal.WindowsIdentity]::GetCurrent()
+if (-not($CurrentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+    Write-Output "It must be run as administrator!"
+    exit
+}
 # Set Execution Polycy
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-
 # Install Chocolatey
 if (-not (Get-Command choco -ea SilentlyContinue)) {
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
@@ -21,13 +16,11 @@ if (-not (Get-Command choco -ea SilentlyContinue)) {
 if (-not (Get-Command git -ea SilentlyContinue)) {
     choco install git.install --yes --params="'/NoShellIntegration /NoAutoCrlf'"
 }
-
 # Clone .dotfiles
 if (!(Test-Path $HOME\.dotfiles)) {
     git clone https://github.com/neko3cs/.dotfiles.git
     Set-Location .dotfiles
 }
-
 # Run PSScripts
 powershell -File .\Set-PwshProfile.ps1
 powershell -File .\Install-ChocolateyApps.ps1
