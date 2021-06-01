c1e7117ba39e58ae98d64fb345c25f23f1e3ba11
neko3cs <36363130+neko3cs@users.noreply.github.com>
2021-06-01 14:20:41 +0900

    "&"がWindows PowerShellで使えないので削除

--- windows-bootstrap.ps1
+++ windows-bootstrap.ps1
@@ -4,8 +4,8 @@
 $title = "clone .dotfiles repo and run bootstrap scripts."
 $message = "ok?: "
 $options = [System.Management.Automation.Host.ChoiceDescription[]](
-    (New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "実行する"),
-    (New-Object System.Management.Automation.Host.ChoiceDescription "&No", "実行しない")
+    (New-Object System.Management.Automation.Host.ChoiceDescription "Yes", "実行する"),
+    (New-Object System.Management.Automation.Host.ChoiceDescription "No", "実行しない")
 )
 $result = $Host.UI.PromptForChoice($title, $message, $options, 1)
 if ($result -ne 0) { exit } 
