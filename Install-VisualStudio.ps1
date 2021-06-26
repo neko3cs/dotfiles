db9c701e9f3ebb2094987187707c462ce88766c6
neko3cs <minaton.0722.net@gmail.com>
2021-06-26 23:07:52 +0900

    インストールスクリプトで勝手にアップデートしないようにする

--- Install-VisualStudio.ps1
+++ Install-VisualStudio.ps1
@@ -37,17 +37,7 @@ if ((vswhere -utf8 -format json | ConvertFrom-Json).Length -eq 0) {
 }
 else {
     Write-Host `
-        "Update Visual Studio ${Edition}...`n" `
-        -ForegroundColor Green
-    Invoke-WebRequest `
-        -Uri http://aka.ms/vs/16/release/$vsInstallerName `
-        -UseBasicParsing `
-        -OutFile $vsInstallerName
-    Start-Process `
-        -FilePath $vsInstallerName `
-        -ArgumentList "update --passive --norestart --wait" `
-        -Verb runas `
-        -Wait
-    Remove-Item `
-        -Path $vsInstallerName
+        "Error: Visual Studio has already installed!" `
+        -ForegroundColor Red
+    exit
 }
